2e9577ffeb9cd2bad47bf3c71282701deb7b5b62
Yuameshi <64469437+Yuameshi@users.noreply.github.com>
2022-04-10 18:19:43 -0700

    完工（

---
+++ headers.js
@@ -0,0 +1,48 @@
+exports.headers = {
+	accept: '*/*',
+	'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
+	'content-type': 'application/x-www-form-urlencoded;charset=UTF-8',
+	'sec-ch-ua':
+		'" Not A;Brand";v="99", "Chromium";v="100", "Google Chrome";v="100.0.1185.36"',
+	'sec-ch-ua-arch': '"x86"',
+	'sec-ch-ua-bitness': '"64"',
+	'sec-ch-ua-full-version': '"100.0.1185.36"',
+	'sec-ch-ua-full-version-list':
+		'" Not A;Brand";v="99.0.0.0", "Chromium";v="100.0.1185.36", "Microsoft Edge";v="100.0.1185.36"',
+	'sec-ch-ua-mobile': '?0',
+	'sec-ch-ua-model': '',
+	'sec-ch-ua-platform': '"Windows"',
+	'sec-ch-ua-platform-version': '"15.0.0"',
+	'sec-fetch-dest': 'empty',
+	'sec-fetch-mode': 'cors',
+	'sec-fetch-site': 'same-origin',
+	'x-goog-batchexecute-bgr':
+		'[";x9m42ZTQAAZHN3W-Zzdf8YwFLPGaRpgmACkAIwj8RmxOqMMqnBr1Z0fPb8ZQrPU5De8XgoV33yk2R-dfoxBLpXMcAx8AAA7CTwAAAAV1AQcXAQIk3bBbpL5SxinBIs1fAEIc4Hg29SLaNSMn3gbhRFHM9CsfJ1jR2svLhFdhehjGNJfqBaZ4hckiRW6NXcGBJCHtmC5JahVRqKInLpECk99Jmrqoc4zTavTT5xucTndO5zbjgkoXQrI_oIX6lnpHgJ2YclAuTDCwuq_5raATCD3KmgV12MaqO-fIhSs7jaDgqMWfvy6frsU6kzNbBUj2P0oYYVWh6xeHM64u9IFytCsBYLAbk5gh4ffWnc9egkjPRZZo34PxJQ307n7uT07gOTEyUgMRheJHcpflvRXY41Gc1Z7eHBGAXgn01vkFcFniAug9lg6DTkse4pXb7nTAlgtS2wWEAjfpOq8tVhRjU7PUNzAQQb_EbHRaZ0RnkOsV8rKu1iczgfkXsCRF0fSl7djzfFUIqKgn8b84nL_Pl6h7lg3DM70D--6NU0gYWYa8wPok-bVuEETGfTqVohkLmw2l1Wm1T6YniQ86FIcNlyORoH729Q4KbkPTlcewMliAo24zKiM-2qLylXaRlxB_p72Hjs8MVSI0M7FFUgPP9IcTzkDlx1jW1IdTOE13RQekGMR9dTMUaCqxiroVETvjfhrAsWlSpjrdpo5U_iV8_TO-mrl9xh-bnTdwaQKfnHjwkb3XL5ycBDQRnGhRXkFk5WHOPFR5YpGWUNJK4i0-lzFq9CfCyXdmKVATNAZd44syAWIimSI0VaJIqFGythQDaZiZSJZwNuuEh0lx2qXc63mFvcUYJvGAayd6ujGB2YtXgDU2m8_g4NTN8xBgdyWzvXVQKkiw98TgEKevl7chDBkNp0ZfjcJjCWJUH9QHNq4Mm0iYefDPTCGQQqWNyLMQRo1G9JWsy88attIFUrEr2GlnzC-HCL_sBr_tU8GTvZeSa6ni-vkIDnCu24nwx50U-VkXMcXQummqqz2OgQ-p3whiBs_h0Crt4F-g052BAxpsIZdR4Z0qzpRBxk-Tj2Wq0Uixix6BlRTcZ9d4j5eU7NXdms8OeaJYZImZAYttSWbsQNwRTmwdVcYY28WFdL7ibVaGpHIMXb_3s4usRgsoegYoUBLNfcnXe_Uwx1JnDtA_8fO985b9tVjlQZiCZhw",null,null,10,null,null,null,0,"2"]',
+	'x-same-domain': '1',
+	cookie: 'NID=511=VyBsZ-SWQ4vJf9RPWSPOrwp7OxKAIwNHHTaBF7Hkoq30J5bhBEBF9JEJailCLYKtgKjK80M34YN3YRVze5aPhvcdcZrgXhD5PovXOrENK0mPUec6LTDjhvNdKlfw1-8FkpsU_Yq4Z4KnRQHGwS1cB6zV--sY8CddEuvHQvlNGsI; _ga=GA1.3.1719117232.1649566504; _gid=GA1.3.338707150.1649566504; OTZ=6454375_24_24__24_',
+	Referer: 'https://translate.google.cn/',
+	'Referrer-Policy': 'origin',
+};
+// exports.headers = {
+// 	accept: '*/*',
+// 	'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
+// 	'sec-ch-ua':
+// 		'" Not A;Brand";v="99", "Chromium";v="100", "Google Chrome";v="100.0.1185.36"',
+// 	'sec-ch-ua-arch': '"x86"',
+// 	'sec-ch-ua-bitness': '"64"',
+// 	'sec-ch-ua-full-version': '"100.0.1185.36"',
+// 	'sec-ch-ua-full-version-list':
+// 		'" Not A;Brand";v="99.0.0.0", "Chromium";v="100.0.1185.36", "Microsoft Edge";v="100.0.1185.36"',
+// 	'sec-ch-ua-mobile': '?0',
+// 	'sec-ch-ua-model': '',
+// 	'sec-ch-ua-platform': '"Windows"',
+// 	'sec-ch-ua-platform-version': '"15.0.0"',
+// 	'sec-fetch-dest': 'empty',
+// 	'sec-fetch-mode': 'cors',
+// 	'sec-fetch-site': 'same-origin',
+// 	'user-agent':
+// 		'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36',
+// 	cookie: '1P_JAR=2022-04-10-05; AEC=AVQQ_LBXSr2u__2ImptY_Q9NKMyXPTenJ88lbPv_brqUM1z0XQ-p2vWINw; OTZ=6454479_24_24__24_; NID=511=UXNzCy723FYnZckwT7Pn6FBhFzYgz6SY32aU2GslpMR_QrJqN4tz61GUJbEDQJKpzcWjEch7oQQgd4Jtv6ggZYNjucgK7vjHBghms5h3O6an2tRW08er155BLCBqcHVnAs5oHevcjRmHAoO93rj5r15O_jXuFnTa2C2U405fkUQ',
+// 	Referer: 'https://translate.google.com/',
+// 	'Referrer-Policy': 'origin',
+// };
---
+++ index.js
@@ -0,0 +1,97 @@
+const translateText = process.argv[2];
+const targetLang = process.argv[3] || 'zh-CN';
+const HttpsProxyAgent = require('https-proxy-agent');
+const fs = require('fs');
+const fetch = require('node-fetch');
+console.log('Translating: ', translateText);
+fs.appendFileSync('translate.log', `[${new Date().toISOString()}][Init] Source: ${translateText}\n`);
+console.log('Target Lang: ', targetLang);
+fs.appendFileSync('translate.log', `[${new Date().toISOString()}][Init] Target Language: ${targetLang}\n`);
+function translate(text, targetLang) {
+	return new Promise(function (resolve) {
+		const translateBody = JSON.stringify([
+			[
+				[
+					'MkEWBc',
+					JSON.stringify([[text, 'auto', targetLang, true], [null]]),
+					null,
+					'generic',
+				],
+			],
+		]);
+		// console.log(translateBody);
+		fetch(
+			'https://translate.google.cn/_/TranslateWebserverUi/data/batchexecute?rpcids=MkEWBc&source-path=%2F&f.sid=-2684615221053975002&bl=boq_translate-webserver_20220406.13_p0&hl=zh-CN&soc-app=1&soc-platform=1&soc-device=1&_reqid=682466&rt=c',
+			{
+				headers: require('./headers.js').headers,
+				referrer: 'https://translate.google.cn/',
+				referrerPolicy: 'origin',
+				agent: require('./proxy.js').proxy
+					? new HttpsProxyAgent(require('./proxy').proxy)
+					: undefined,
+				body: 'f.req=' + encodeURIComponent(translateBody),
+				method: 'POST',
+				mode: 'cors',
+				credentials: 'include',
+			}
+		).then((res) => {
+			res.text().then((text) => {
+				const lines = text.split('\n');
+				lines.forEach((line) => {
+					try {
+						// console.log(line);
+						const json = JSON.parse(line)[0][2];
+						const result = JSON.parse(json)[1][0][0][5][0][0];
+						resolve(result);
+					} catch (error) {
+						undefined;
+						//do nothing
+					}
+				});
+				// const result=JSON.parse(JSON.parse(lines[3])[0])[1][0][0][5][0][0];
+				// console.log(result);
+				// resolve(json[0][0][0]);
+			});
+		});
+		// fetch(
+		// 	`https://translate.google.com/translate_a/single?client=at&sl=auto&tl=${targetLang}&dt=t&q=${text}`,
+		// 	{
+		// 		headers: require('./headers.js').headers,
+		// 		agent: require('./proxy.js').proxy
+		// 			? new HttpsProxyAgent(require('./proxy').proxy)
+		// 			: undefined,
+		// 		referrer: 'https://translate.google.com/',
+		// 		referrerPolicy: 'origin',
+		// 		body: null,
+		// 		method: 'GET',
+		// 		mode: 'cors',
+		// 		credentials: 'include',
+		// 	}
+		// ).then((res) => {
+		// 	res.json().then((json) => {
+		// 		resolve(json[0][0][0]);
+		// 	});
+		// });
+	});
+}
+function parseGoogleTranslateCallback(object) {
+	return JSON.parse(JSON.parse(object)[0][2])[1][0][0][5][0][0];
+}
+const langs = require('./langs.js').langs;
+let outputText = translateText;
+for (let i = 0; i < langs.length; i++) {
+	setTimeout(() => {
+		translate(outputText, langs[i]).then((res) => {
+			fs.appendFileSync('translate.log', `[${new Date().toISOString()}][Translate] [Loop:${i}]	[To ${langs[i]}]	Result: ${res}\n`);
+			outputText = res;
+			console.log(outputText);
+		});
+	}, 5000 * i);
+}
+setTimeout(() => {
+	translate(outputText, targetLang).then((res) => {
+		fs.appendFileSync('translate.log', `[${new Date().toISOString()}][Translate] [Ending]	[To ${targetLang}]	Result: ${res}\n`);
+		outputText = res;
+		console.log(outputText);
+	});
+}, 5000 * (langs.length + 1));
