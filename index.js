2e376f8c6847397003e7c7c901a3cf2c6471e7bb
Hua <70087243+AyaneYe@users.noreply.github.com>
2022-10-16 01:52:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -31,62 +31,62 @@ function translate(text, targetLang) {
 			],
 		]);
 		// console.log(translateBody);
+// 		fetch(
+// 			'https://translate.google.com/_/TranslateWebserverUi/data/batchexecute?rpcids=MkEWBc&source-path=%2F&f.sid=-2684615221053975002&bl=boq_translate-webserver_20220406.13_p0&hl=zh-CN&soc-app=1&soc-platform=1&soc-device=1&_reqid=682466&rt=c',
+// 			{
+// 				headers: require('./headers.js').headers,
+// 				referrer: 'https://translate.google.com/',
+// 				referrerPolicy: 'origin',
+// 				agent: require('./proxy.js').proxy
+// 					? new HttpsProxyAgent(require('./proxy').proxy)
+// 					: undefined,
+// 				body: 'f.req=' + encodeURI(translateBody),
+// 				method: 'POST',
+// 				mode: 'cors',
+// 				credentials: 'include',
+// 			}
+// 		).then((res) => {
+// 			res.text().then((text) => {
+// 				const lines = text.split('\n');
+// 				lines.forEach((line) => {
+// 					try {
+// 						// console.log(line);
+// 						const json = JSON.parse(line)[0][2];
+// 						const translations = JSON.parse(json)[1][0][0][5];
+// 						let result='';
+// 						translations.forEach((translation) => {
+// 							result=result+translation[0];
+// 						});
+// 						resolve(result);
+// 					} catch (error) {
+// 						undefined;
+// 						//do nothing
+// 					}
+// 				});
+// 				// const result=JSON.parse(JSON.parse(lines[3])[0])[1][0][0][5][0][0];
+// 				// console.log(result);
+// 				// resolve(json[0][0][0]);
+// 			});
+// 		});
 		fetch(
-			'https://translate.google.com/_/TranslateWebserverUi/data/batchexecute?rpcids=MkEWBc&source-path=%2F&f.sid=-2684615221053975002&bl=boq_translate-webserver_20220406.13_p0&hl=zh-CN&soc-app=1&soc-platform=1&soc-device=1&_reqid=682466&rt=c',
+			`https://translate.google.com/translate_a/single?client=at&sl=auto&tl=${targetLang}&dt=t&q=${text}`,
 			{
 				headers: require('./headers.js').headers,
-				referrer: 'https://translate.google.com/',
-				referrerPolicy: 'origin',
 				agent: require('./proxy.js').proxy
 					? new HttpsProxyAgent(require('./proxy').proxy)
 					: undefined,
-				body: 'f.req=' + encodeURI(translateBody),
-				method: 'POST',
+				referrer: 'https://translate.google.com/',
+				referrerPolicy: 'origin',
+				body: null,
+				method: 'GET',
 				mode: 'cors',
 				credentials: 'include',
 			}
 		).then((res) => {
-			res.text().then((text) => {
-				const lines = text.split('\n');
-				lines.forEach((line) => {
-					try {
-						// console.log(line);
-						const json = JSON.parse(line)[0][2];
-						const translations = JSON.parse(json)[1][0][0][5];
-						let result='';
-						translations.forEach((translation) => {
-							result=result+translation[0];
-						});
-						resolve(result);
-					} catch (error) {
-						undefined;
-						//do nothing
-					}
-				});
-				// const result=JSON.parse(JSON.parse(lines[3])[0])[1][0][0][5][0][0];
-				// console.log(result);
-				// resolve(json[0][0][0]);
+			res.json().then((json) => {
+				resolve(json[0][0][0]);
 			});
 		});
-		// fetch(
-		// 	`https://translate.google.com/translate_a/single?client=at&sl=auto&tl=${targetLang}&dt=t&q=${text}`,
-		// 	{
-		// 		headers: require('./headers.js').headers,
-		// 		agent: require('./proxy.js').proxy
-		// 			? new HttpsProxyAgent(require('./proxy').proxy)
-		// 			: undefined,
-		// 		referrer: 'https://translate.google.com/',
-		// 		referrerPolicy: 'origin',
-		// 		body: null,
-		// 		method: 'GET',
-		// 		mode: 'cors',
-		// 		credentials: 'include',
-		// 	}
-		// ).then((res) => {
-		// 	res.json().then((json) => {
-		// 		resolve(json[0][0][0]);
-		// 	});
-		// });
 	});
 }
 const langs = require('./langs.js').langs;
